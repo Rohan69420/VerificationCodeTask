80cdc007bc82ae91df6db4e259e7093493ed9e41
Rohan <rohan13maharjan@gmail.com>
2024-10-23 12:08:54 +0545

    Made changes with functional pattern concepts

--- client/src/components/code-input.jsx
+++ client/src/components/code-input.jsx
@@ -15,44 +15,68 @@ const CodeInput = ({ length = 6, onCodeSubmit = () => {} }) => {
 
   //Submit button function
   const onSubmitClick = () => {
-    //Empty values check
-
+    //Check if any empty fields
     if (code.indexOf("") >= 0) return;
+
     onCodeSubmit(code);
   };
 
-  const handleChange = (index, event) => {
-    const value = event.target.value;
+  const validityMarker = (value, index) => {
+    // avoid in-line css changes
     if (isNaN(value)) {
-      // Client-side validation for Non-Numeric Value; Change color to red
+      // mark invalid
       inputRefs.current[index].style.outline = "2px solid red";
-      return;
+      return false;
+    } else {
+      // valid marker : clear the style
+      inputRefs.current[index].style.outline = "";
+      return true;
     }
+  };
 
-    //Reset the style of the input box if numeric
-    inputRefs.current[index].style.outline = "";
-
+  const inputParser = (value) => {
     const newCode = [...code];
 
-    //Validation for only one input per input field
+    //Only taking one input at a time
     newCode[index] = value.substring(value.length - 1);
     setCode(newCode);
 
-    //Flash combined code on console after length of 6 is reached
+    // Debug \|/
     const combineCode = newCode.join("");
     if (combineCode.length === length) {
       console.log(combineCode);
     }
+  };
 
+  const moveCursor = (index, value) => {
     //Auto move to next input field
     if (value && index < length - 1 && inputRefs.current[index + 1]) {
       inputRefs.current[index + 1].focus();
     }
   };
 
+  const handleChange = (index, event) => {
+    /* Ideally we would want the functions to be as follows:
+      > validityHandler
+      > inputParser <--- can have the added functionality to handle clipboard data
+      > immediateNextFieldSelector <--- select the last field if complete or skip to the length-size
+    */
+    const value = event.target.value;
+
+    if (!validityMarker(value, index)) {
+      // don't call the remaining functions if invalid
+      return;
+    }
+    inputParser(value);
+    moveCursor(value, index);
+  };
+
   const handleClick = (index) => {
     inputRefs.current[index].setSelectionRange(1, 1);
+    selectFirstEmptyField(index);
+  };
 
+  const selectFirstEmptyField = (index) => {
     //Auto move on click to the earliest encounter of empty field; leave no spaces in between
     if (index > 0 && !code[index - 1]) {
       inputRefs.current[code.indexOf("")].focus();
@@ -73,24 +97,24 @@ const CodeInput = ({ length = 6, onCodeSubmit = () => {} }) => {
   return (
     <div className="container">
       <div>
-      {code.map((value, index) => {
-        return (
-          <input
-            key={index}
-            type="text"
-            ref={(input) => {
-              inputRefs.current[index] = input;
-            }}
-            value={value}
-            onChange={(event) => handleChange(index, event)}
-            onClick={() => handleClick(index)}
-            onKeyDown={(event) => handleKeyDown(index, event)}
-            className="code-field"
-          />
-        );
-      })}
+        {code.map((value, index) => {
+          return (
+            <input
+              key={index}
+              type="text"
+              ref={(input) => {
+                inputRefs.current[index] = input;
+              }}
+              value={value}
+              onChange={(event) => handleChange(index, event)}
+              onClick={() => handleClick(index)}
+              onKeyDown={(event) => handleKeyDown(index, event)}
+              className="code-field"
+            />
+          );
+        })}
       </div>
-      
+
       <div>
         <button className="submit-button" onClick={onSubmitClick}>
           Submit
