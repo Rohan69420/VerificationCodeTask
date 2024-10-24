bcdec713ddf1aa5508dbd3b4d3856c6ca389b5bb
Rohan <rohan13maharjan@gmail.com>
2024-10-24 14:20:29 +0545

    Changes to use pages instead of components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import RequestCard from "./components/request-verification";
+import Verification from "./pages/VerificationV2.jsx";
 
 function App() {
 
   return (
     <div className="App">
       <h1 className="h1"> Verify Your Credentials</h1>
-      <RequestCard />
+      {/* <RequestCard /> */}
+      <Verification/>
     </div>
   );
 }
